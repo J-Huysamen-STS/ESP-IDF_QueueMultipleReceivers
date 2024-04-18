e6ab81dd3921e014bb598807e15f265261857b5a
Joshua Huysamen <josh@seatechnology.co.za>
2024-04-18 14:55:53 +0200

    only 1 led task works at a time. Fix issue.

--- src/main.c
+++ src/main.c
@@ -11,8 +11,13 @@
 #define LED_RED_MASK (1ULL << LED1_RED)
 #define LED_ORANGE_MASK (1ULL << LED2_ORANGE)
 
-#define PUSH_BUTTON_1 GPIO_NUM_12
+#define PUSH_BUTTON_1 GPIO_NUM_25
+#define PUSH_BUTTON_2 GPIO_NUM_26
+#define PUSH_BUTTON_3 GPIO_NUM_27
+
 #define PUSH_BUTTON_1_MASK (1ULL << PUSH_BUTTON_1)
+#define PUSH_BUTTON_2_MASK (1ULL << PUSH_BUTTON_2)
+#define PUSH_BUTTON_3_MASK (1ULL << PUSH_BUTTON_3)
 
 #define NUM_OF_DATA 15
 
@@ -26,46 +31,45 @@ void config_gpio(void);
 void Led1_task(void *parameter);
 void Led2_task(void *parameter);
 void Led3_task(void *parameter);
-void Send_to_led(void *parameters);
+
+void Send_to_led1(void *parameters);
+void Send_to_led2(void *parameters);
+void Send_to_led3(void *parameters);
 
 // create an array to pass values to the LED strips. Each value corressponds to the number of blinks to be made.
-int blink_led_values[NUM_OF_DATA] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+int blink_led1_values[NUM_OF_DATA] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+int blink_led2_values[NUM_OF_DATA] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+int blink_led3_values[NUM_OF_DATA] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 
-QueueHandle_t myQueue;
+QueueHandle_t myQueue1;
+QueueHandle_t myQueue2;
+QueueHandle_t myQueue3;
 
 
-const TickType_t xDelay = (1000 / portTICK_PERIOD_MS);
+const TickType_t xDelay = (500 / portTICK_PERIOD_MS);
+const TickType_t xDelay2 = (500 / portTICK_PERIOD_MS);
+const TickType_t xDelay3 = (500 / portTICK_PERIOD_MS);
 
 
 void app_main() {
-    // int LED1 = LED1_RED;
-    // int LED2 = LED2_ORANGE;
-    // int LED3 = LED3_GREEN;
     
     config_gpio();
 
-    myQueue = xQueueCreate(10, sizeof(int));
+    myQueue1 = xQueueCreate(10, sizeof(int));
+    myQueue2 = xQueueCreate(10, sizeof(int));
+    myQueue3 = xQueueCreate(10, sizeof(int));
 
     xTaskCreate(Led1_task, "led1_task", 1048, NULL, 1, NULL);
     xTaskCreate(Led2_task, "led2_task", 1048, NULL, 1, NULL);
     xTaskCreate(Led3_task, "led3_task", 1048, NULL, 1, NULL);
     
-    xTaskCreate(Send_to_led, "send delay to led", 1048, NULL, 2, NULL);
-
+    xTaskCreate(Send_to_led1, "send command to led1", 1048, NULL, 2, NULL);
+    xTaskCreate(Send_to_led2, "send command to led2", 1048, NULL, 2, NULL);
+    xTaskCreate(Send_to_led3, "send command to led3", 1048, NULL, 2, NULL);
 
     while (1)
     {
-        /*if (gpio_get_level(PUSH_BUTTON_1) == 0)
-        {
-            LED_switch(&LED1, &LED2, &LED3, 1, 0, 0);
-            vTaskDelay(xDelay);
-            LED_switch(&LED1, &LED2, &LED3, 0, 1, 0);
-            vTaskDelay(xDelay);
-            LED_switch(&LED1, &LED2, &LED3, 0, 0, 1);
-            vTaskDelay(xDelay);
-            LED_switch(&LED1, &LED2, &LED3, 0, 0, 0);
-        }
-        */
+        // Empty
     }
     
 }
@@ -81,7 +85,7 @@ void config_gpio(void){
     gpio_config(&LED_GPIOConfig);
 
     // Define Push-Button GPIO Config
-    PB_GPIOConfig.pin_bit_mask  = (PUSH_BUTTON_1_MASK);             // Pin D12.
+    PB_GPIOConfig.pin_bit_mask  = (PUSH_BUTTON_1_MASK | PUSH_BUTTON_2_MASK | PUSH_BUTTON_3_MASK);
     PB_GPIOConfig.mode          = GPIO_MODE_INPUT;
     PB_GPIOConfig.pull_up_en    = GPIO_PULLUP_ENABLE;
     PB_GPIOConfig.pull_down_en  = GPIO_PULLDOWN_DISABLE;
@@ -92,13 +96,6 @@ void config_gpio(void){
     // gpio_set_direction(LED3_GREEN, GPIO_MODE_OUTPUT);
 }
 
-/*
-void LED_switch(int *_LED1, int *_LED2, int *_LED3, bool _LED1_state, bool _LED2_state, bool _LED3_state){
-    gpio_set_level(*_LED1, _LED1_state);
-    gpio_set_level(*_LED2, _LED2_state);
-    gpio_set_level(*_LED3, _LED3_state);
-}
-*/
 
 void Led1_task(void *parameter){
     int led_blink = 0;
@@ -106,7 +103,7 @@ void Led1_task(void *parameter){
 
     while (1)
     {
-        if (xQueueReceive(myQueue, &led_blink, 0) == pdTRUE)
+        if (xQueueReceive(myQueue1, &led_blink, 0) == pdTRUE)
         {
             for (i = 0; i < led_blink; i++)
             {
@@ -114,14 +111,14 @@ void Led1_task(void *parameter){
                 vTaskDelay(xDelay);
                 gpio_set_level(LED1_RED, 0);
                 vTaskDelay(xDelay);
-                gpio_set_level(LED1_RED, 1);
-                vTaskDelay(xDelay);
-                gpio_set_level(LED1_RED, 0);
-                vTaskDelay(xDelay);
-                gpio_set_level(LED1_RED, 1);
-                vTaskDelay(xDelay);
-                gpio_set_level(LED1_RED, 0);
-                vTaskDelay(xDelay);
+                // gpio_set_level(LED1_RED, 1);
+                // vTaskDelay(xDelay);
+                // gpio_set_level(LED1_RED, 0);
+                // vTaskDelay(xDelay);
+                // gpio_set_level(LED1_RED, 1);
+                // vTaskDelay(xDelay);
+                // gpio_set_level(LED1_RED, 0);
+                // vTaskDelay(xDelay);
             }
         }
         else
@@ -140,22 +137,22 @@ void Led2_task(void *parameter){
 
     while (1)
     {
-        if (xQueueReceive(myQueue, &led_blink, 0) == pdTRUE)
+        if (xQueueReceive(myQueue2, &led_blink, 0) == pdTRUE)
         {
             for (i = 0; i < led_blink; i++)
             {
                 gpio_set_level(LED2_ORANGE, 1);
-                vTaskDelay(xDelay);
+                vTaskDelay(xDelay2);
                 gpio_set_level(LED2_ORANGE, 0);
-                vTaskDelay(xDelay);
-                gpio_set_level(LED2_ORANGE, 1);
-                vTaskDelay(xDelay);
-                gpio_set_level(LED2_ORANGE, 0);
-                vTaskDelay(xDelay);
-                gpio_set_level(LED2_ORANGE, 1);
-                vTaskDelay(xDelay);
-                gpio_set_level(LED2_ORANGE, 0);
-                vTaskDelay(xDelay);
+                vTaskDelay(xDelay2);
+                // gpio_set_level(LED2_ORANGE, 1);
+                // vTaskDelay(xDelay);
+                // gpio_set_level(LED2_ORANGE, 0);
+                // vTaskDelay(xDelay);
+                // gpio_set_level(LED2_ORANGE, 1);
+                // vTaskDelay(xDelay);
+                // gpio_set_level(LED2_ORANGE, 0);
+                // vTaskDelay(xDelay);
             }
         }
         else
@@ -174,22 +171,22 @@ void Led3_task(void *parameter){
 
     while (1)
     {
-        if (xQueueReceive(myQueue, &led_blink, 0) == pdTRUE)
+        if (xQueueReceive(myQueue3, &led_blink, 0) == pdTRUE)
         {
             for (i = 0; i < led_blink; i++)
             {
                 gpio_set_level(LED3_GREEN, 1);
-                vTaskDelay(xDelay);
+                vTaskDelay(xDelay3);
                 gpio_set_level(LED3_GREEN, 0);
-                vTaskDelay(xDelay);
-                gpio_set_level(LED3_GREEN, 1);
-                vTaskDelay(xDelay);
-                gpio_set_level(LED3_GREEN, 0);
-                vTaskDelay(xDelay);
-                gpio_set_level(LED3_GREEN, 1);
-                vTaskDelay(xDelay);
-                gpio_set_level(LED3_GREEN, 0);
-                vTaskDelay(xDelay);
+                vTaskDelay(xDelay3);
+                // gpio_set_level(LED3_GREEN, 1);
+                // vTaskDelay(xDelay);
+                // gpio_set_level(LED3_GREEN, 0);
+                // vTaskDelay(xDelay);
+                // gpio_set_level(LED3_GREEN, 1);
+                // vTaskDelay(xDelay);
+                // gpio_set_level(LED3_GREEN, 0);
+                // vTaskDelay(xDelay);
             }
         }
         else
@@ -202,7 +199,7 @@ void Led3_task(void *parameter){
 }
 
 
-void Send_to_led(void *parameters){
+void Send_to_led1(void *parameters){
     int i = 0;
 
     while (1)
@@ -211,8 +208,8 @@ void Send_to_led(void *parameters){
         {
             if (i < NUM_OF_DATA)
             {
-                xQueueSend(myQueue, &blink_led_values[i], 10);
-                vTaskDelay(300/portTICK_PERIOD_MS);     // Debounce delay
+                xQueueSend(myQueue1, &blink_led1_values[i], 10);
+                vTaskDelay(200/portTICK_PERIOD_MS);     // Debounce delay
                 i++;
             }else
             {
@@ -221,5 +218,48 @@ void Send_to_led(void *parameters){
         }
         //vTaskDelay(10);
     }
-    
+}
+
+
+void Send_to_led2(void *parameters){
+    int i = 0;
+
+    while (1)
+    {
+        if (gpio_get_level(PUSH_BUTTON_2) == 0)
+        {
+            if (i < NUM_OF_DATA)
+            {
+                xQueueSend(myQueue2, &blink_led2_values[i], 10);
+                vTaskDelay(200/portTICK_PERIOD_MS);     // Debounce delay
+                i++;
+            }else
+            {
+                i = 0;
+            }
+        }
+        //vTaskDelay(10);
+    }
+}
+
+
+void Send_to_led3(void *parameters){
+    int i = 0;
+
+    while (1)
+    {
+        if (gpio_get_level(PUSH_BUTTON_3) == 0)
+        {
+            if (i < NUM_OF_DATA)
+            {
+                xQueueSend(myQueue3, &blink_led3_values[i], 10);
+                vTaskDelay(200/portTICK_PERIOD_MS);     // Debounce delay
+                i++;
+            }else
+            {
+                i = 0;
+            }
+        }
+        //vTaskDelay(10);
+    }
 }
